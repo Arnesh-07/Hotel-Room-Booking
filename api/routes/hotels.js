978f9f252418156230eb1ed14dd4aa3424b4884a
Arnesh-07 <arne21107.it@rmkec.ac.in>
2024-01-21 11:06:14 +0530

    booking

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,18 +1,19 @@
 import express from "express"
 import Hotel from "../models/Hotel.js";
+import { createError } from "../utils/error.js";
+import { createHotel, deleteHotel, getHotel, getHotels, updatedHotel } from "../controllers/hotel.js";
 
 const router=express.Router();
 
-router.post("/", async (req,res)=>{
-    
-    const newHotel = new Hotel(req.body)
-    
-    try{
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-    }catch(error){
-        res.status(500).json(error)
-    }
-})
+router.post("/",createHotel);
 
-export default router
+router.put("/:id", updatedHotel);
+
+router.delete("/:id",deleteHotel);
+
+router.get("/:id",getHotel);
+
+
+router.get("/",getHotels);
+
+export default router;
--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,7 +1,19 @@
-import express from "express"
+import express from "express";
+import {
+  updateUser,
+  deleteUser,
+  getUser,
+  getUsers,
+} from "../controllers/user.js";
 
-const router=express.Router();
+const router = express.Router();
 
+router.put("/:id",updateUser);
 
+router.delete("/:id",deleteUser);
 
-export default router
+router.get("/:id",getUser);
+
+router.get("/",getUsers);
+
+export default router;
